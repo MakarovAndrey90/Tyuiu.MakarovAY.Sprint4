8ab6fd42501cd12c0b21d7e21f48effe75954489
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-11-14 12:20:26 +0500

    Task6

---
+++ Tyuiu.MakarovAY.Sprint4.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MakarovAY.Sprint4.Task6.V13.Lib;
+namespace Tyuiu.MakarovAY.Sprint4.Task6.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string[] g = new string[] { "Ford", "Toyota", "Honda", "Chevrolet", "Mercedes", "BMW", "Audi" };
+            int res = ds.Calculate(g);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint4.Task6.V13/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.MakarovAY.Sprint4.Task6.V13.Lib;
+namespace Tyuiu.MakarovAY.Sprint4.Task6.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных подсчитайте количество элементов, длина     *");
+            Console.WriteLine("* которых больше 4.                                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string[] g = new string[] { "Ford", "Toyota", "Honda", "Chevrolet", "Mercedes", "BMW", "Audi" };
+            Console.WriteLine("");
+            for (int i = 0; i < g.Length -1; i++)
+            {
+                Console.WriteLine(g[i]);
+            }
+
+
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine(" Количество элементов длина которых больше 7 = " + ds.Calculate(g));
+                Console.ReadKey();
+            }
+    }
+}
