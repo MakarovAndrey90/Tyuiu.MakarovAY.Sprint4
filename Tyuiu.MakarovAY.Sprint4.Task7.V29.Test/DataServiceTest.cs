abcfbaa060854aa6d3bcfa464ad6ab14fa602c36
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-11-14 21:01:53 +0500

    Task7

---
+++ Tyuiu.MakarovAY.Sprint4.Task7.V29.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MakarovAY.Sprint4.Task7.V29.Lib;
+namespace Tyuiu.MakarovAY.Sprint4.Task7.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            int n = 4;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            string str = "983157642891";
+            DataService ds = new DataService();
+            int res = ds.Calculate(n, m, str);
+            int wait = 28;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint4.Task7.V29/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.MakarovAY.Sprint4.Task7.V29.Lib;
+namespace Tyuiu.MakarovAY.Sprint4.Task7.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов                            *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 983157642891 Преобразуйте ее          *");
+            Console.WriteLine("* в матрицу 4 на 3 и подсчитайте сумму четных чисел.                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int rows = 4;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "983157642891";
+            int sum = 0;
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, columns, str);
+            Console.WriteLine("Сумма четных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
